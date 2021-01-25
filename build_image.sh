3daad2d2939589fec9b57f82b7a1794c8cd6fcd5
Okadarien Saru <okadas@tanban.org>
2021-01-26 00:49:11 +0900

    reduce verbosity of build script

--- build_image.sh
+++ build_image.sh
@@ -1,6 +1,10 @@
-#!/bin/sh -ex
-docker_user=${DOCKER_USERNAME:-localhost}
-cd Dockerfiles
+#!/bin/sh -e
+
+case "$1" in
+-x) set -o xtrace;
+-v) set -o verbose;
+*) :;;
+esac
 
 build_atomic(){
   ${JUST_SAYING:+echo} docker build -t "${docker_user:+$docker_user/}${shell}:${baseimg:?baseimg corrupted}" -f $1 .
@@ -12,6 +16,9 @@ push_atomic(){
   [ "${baseimg}" = debian-buster-slim ] && ${JUST_SAYING:+echo} docker push ${docker_user:+$docker_user/}${shell}:latest || return 0
 }
 
+main(){
+docker_user=${DOCKER_USERNAME:-localhost}
+cd Dockerfiles
 for i in */*; do
   docker_opts=""
   shell=${i##*/}
@@ -28,3 +35,6 @@ for i in */*; do
     ${DOCKER_PUSHING:+ push_atomic};;
   esac
 done
+}
+
+main
