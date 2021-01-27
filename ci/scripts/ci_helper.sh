46a733ee9e8cbf1e964a5c2dc9ce019d204b1a06
Okadarien Saru <okadas@tanban.org>
2021-01-27 22:39:47 +0900

    simple test script added

--- ci/scripts/ci_helper.sh
+++ ci/scripts/ci_helper.sh
@@ -23,11 +23,14 @@ ci_get_img_full(){
 }
 
 ci_conv2tap(){
-  awk '{
-    if ($NF == "ok"){ printf("%s %s ",$1,NR); for(i=2;i<NF;i++){ printf("%s",$i);};}
-    else { printf("not ok %s %s",NR,$0); } 
-    printf("\n");
-  }' ${1:?no target file!} 
+  cat ${1:?no target file!} \
+    | awk '{
+        if ($NR == 1){ print "1..@@@@@@@@@@"; printf("%s %s ",$1,NR); for(i=2;i<NF;i++){ printf("%s",$i);};}
+        if ($NF == "ok"){ printf("%s %s ",$1,NR); for(i=2;i<NF;i++){ printf("%s",$i);};}
+        else { printf("not ok %s %s",NR,$0); } 
+        printf("\n");
+      }' \
+    | awk "{ gsub(/@@@@@@@@@@/,$(cat $1 | wc -l)); print; }"
 }
 
 foreach_dockerfile(){ #function
---
+++ test_images.sh
@@ -0,0 +1,1 @@
+ci/scripts/test_images.sh
